614ff67722097ec80ca43a49ad1621972ca3cbda
Joey Marianer <me@joeym.org>
2026-03-14 04:26:54 -0500

    Main page with a dialog that doesn't do much

---
+++ src/lib/MainView.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import SettingsDialog from '$lib/SettingsDialog.svelte';
+
+  let settingsDialog: SettingsDialog;
+</script>
+
+<div class="main-view">
+  <button onclick={() => settingsDialog.open()}>Settings</button>
+  <SettingsDialog bind:this={settingsDialog} />
+</div>
+
+<style>
+  .main-view {
+    background-color: deeppink;
+    padding: 1rem;
+    height: 100vh;
+  }
+</style>
---
+++ src/lib/SettingsDialog.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+  let dialog: HTMLDialogElement;
+
+  export function open() {
+    dialog.showModal();
+  }
+
+  function close() {
+    dialog.close();
+  }
+</script>
+
+<dialog bind:this={dialog}>
+  <h2>Settings</h2>
+  <p>Hi, I'm a dialog.</p>
+  <div class="buttons">
+    <button onclick={close}>Save</button>
+    <button onclick={close}>Cancel</button>
+  </div>
+</dialog>
+
+<style>
+  dialog::backdrop {
+    background: rgba(0, 0, 0, 0.5);
+  }
+
+  dialog[open] {
+    display: flex;
+    margin: auto;
+    border-radius: 5vmin;
+    border: 1vmin solid rgba(0,0,0,0.8);
+    box-shadow: 0.2vmin 0.2vmin rgba(50,50,50,0.8);
+    flex-direction: column;
+    outline: 0.5vmin solid #FFF;
+    min-width: 50vmin;
+    font-family: BigNoodleTitling;
+
+    h2 {
+        text-transform: uppercase;
+        font-size: 2em;
+        width: 100%;
+        border-bottom: 1vmin solid rgba(0,0,0,0.8);
+        text-align: center;
+    }
+
+    p {
+        padding: 1rem;
+    }
+
+    .buttons {
+        display: flex;
+        justify-content: space-evenly;
+        padding-bottom: 0.5rem;
+    }
+
+    button {
+        border: 0.5vmin solid #000;
+        text-align: center;
+        border-radius: 1vmin;
+        min-width: 20vmin;
+        font-size: 1.5em;
+
+        &:hover {
+            background-color: #DDD;
+        }
+    }
+  }
+</style>
